7f5236cdb1f2de33dc5d1ff0fa3ddfc4b94aba49
Stanley Stevens <stanleystevens@Stanleys-MacBook-Pro.local>
2015-10-21 15:58:30 -0700

    fixed errors thus far

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -27,6 +27,7 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
+    console.log("inside getPlayerMove");
     return PlayerMove = move || getInput();
 }
 
@@ -36,6 +37,7 @@ function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+    console.log("inside getComputerMove");
     return ComputerMove = move || randomPlay();
 }
 
@@ -44,38 +46,43 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    if (PlayerMove === "rock" && ComputerMove === "scissors") 
+    console.log("inside winner pre conditions");
+    if (playerMove === "rock" && computerMove === "scissors") {
         winner = "player";
-    } else if (PlayerMove === "rock" && ComputerMove === "rock") {
+    } else if (playerMove === "rock" && computerMove === "rock") {
         winner = "tie";
-    } else if (PlayerMove === "rock" && ComputerMove === "paper") {
+    } else if (playerMove === "rock" && computerMove === "paper") {
         winner = "computer";
-    } else if (PlayerMove === "paper" && ComputerMove === "scissors") {
+    } else if (playerMove === "paper" && computerMove === "scissors") {
         winner = "computer";
-    } else if (PlayerMove === "paper" && ComputerMove === "rock") {
+    } else if (playerMove === "paper" && computerMove === "rock") {
         winner = "player";
-    } else if (PlayerMove === "paper" && ComputerMove === "paper") {
+    } else if (playerMove === "paper" && computerMove === "paper") {
         winner = "tie";
-    } else if (PlayerMove === "scissors" && ComputerMove === "scissors") {
+    } else if (playerMove === "scissors" && computerMove === "scissors") {
         winner = "tie";
-    } else if (PlayerMove === "scissors" && ComputerMove === "rock") {
+    } else if (playerMove === "scissors" && computerMove === "rock") {
         winner = "computer";
-    } else if (PlayerMove === "scissors" && ComputerMove === "paper") {
+    } else if (playerMove === "scissors" && computerMove === "paper") {
         winner = "player";
     } else {
         winner = "bad input, needs to be R, P or S.";
     }
+    console.log("inside winner pre winner");
     return winner;
 }
 
 function playToFive() {
+    var result;
     var playerWins = 0;
     var computerWins = 0;
     var ties = 0;
-    while (playerWins < 5 && computerWins < 5) {
+    while ((playerWins < 5) && (computerWins < 5)) {
         console.log("Let's play Rock, Paper, Scissors");
         // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-        result = getWinner(getPlayerMove(),getComputerMove())
+        console.log("inside playToFive pre result");
+        result = getWinner(getPlayerMove(),getComputerMove());
+        console.log(result);
         if (result === "player"){
             playerWins += 1;
         } else if (result === "computer") {
@@ -83,7 +90,11 @@ function playToFive() {
         } else {
             ties += 1;
         }
-        return [playerWins, computerWins];
+        console.log("pre array");
+        console.log("Player: " + playerWins);
+        console.log("Computer: " + computerWins);
     }
+    return [playerWins, computerWins];
 }
 
+playToFive();
